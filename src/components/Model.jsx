f9e24dace2d799e79036a7dde88b20085b3221c6
Arthur Prudente <91214785+arthurprudenteres@users.noreply.github.com>
2023-02-23 14:40:49 -0300

    fix:

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { FiChevronDown } from "react-icons/fi";
 
 function Model() {
-
   return (
     <div>
       <div className="absolute inset-x-0 top-[15%] text-center">
@@ -15,11 +14,9 @@ function Model() {
             Custom Order
           </button>
         </a>
-        <a href="https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD?quality=auto-medium&amp;format=auto">
-          <button className="bg-[#f4f4f4] rounded-md w-96 lg:w-64 lg:mx-4 h-10 mt-2">
-            Demo Drive
-          </button>
-        </a>
+        <button className="bg-[#f4f4f4] rounded-md w-96 lg:w-64 lg:mx-4 h-10 mt-2">
+          Demo Drive
+        </button>
       </div>
       <div className="flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]">
         <FiChevronDown size={24} />
